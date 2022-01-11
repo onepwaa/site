9cbbd5e37422765c1f7ecad0ecc074aa7058468f
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-01-11 16:20:09 +1100

    Save 1 Quick

---
+++ my-app/src/routes/index.svelte
@@ -0,0 +1,42 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+<div class="flex flex-col min-h-screen">
+	<div class="bg-blue-900 py-3 px-4 text-white font-bold flex items-center">
+		<h3 class="flex-grow">Join Us On 29th January for the What The Pwaa! Debate Tournament</h3>
+		<h3 class="text-5xl close font-normal">+</h3>
+	</div>
+	<div class="bg-gray-900 py-4 px-4">
+		<img src="img/onepwaa-logo-black.png" class="h-10" />
+	</div>
+	<div class="flex flex-col flex-grow" id="menu">
+		<div class="bg-theme-red section">
+			<h2 class="home-heading-text flex-grow">Study Guides</h2>
+			<h2 class="home-heading-text chevorn italic">&gt;&gt;&gt;</h2>
+		</div>
+		<div class="bg-theme-green section">
+			<h2 class="home-heading-text flex-grow">Deep Dives</h2>
+			<h2 class="home-heading-text chevorn">&gt;&gt;&gt;</h2>
+		</div>
+		<div class="bg-theme-peach section">
+			<h2 class="home-heading-text flex-grow">Strategy</h2>
+			<h2 class="home-heading-text chevorn">&gt;&gt;&gt;</h2>
+		</div>
+		<div class="bg-theme-purple section">
+			<h2 class="home-heading-text flex-grow">Humans of WSC</h2>
+			<h2 class="home-heading-text chevorn">&gt;&gt;&gt;</h2>
+		</div>
+		<div class="bg-theme-blue section" onclick="closeMenu()">
+			<h2 class="home-heading-text flex-grow">About Us</h2>
+			<h2 class="home-heading-text chevorn">&gt;&gt;&gt;</h2>
+		</div>
+	</div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./*.{html,js,css}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
